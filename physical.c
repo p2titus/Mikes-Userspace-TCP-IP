b61c9aab3fb666aebbe01e2d278fffbccad85cf6
mike <michael.hallam3@gmail.com>
2021-08-16 21:45:19 +0100

    unsure of how physical file will work yet - it'll be done when it's done'

--- physical.c
+++ physical.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
+//#include<syshead.h>
+//#include<utils.h>
+#include<basic.h>
 
-void send_data(int* errorno) {
-	*errorno = -1;
+// using Linux TAP for intercepting low level traffic
+// essentially, this class is a bit of a hack to get the traffic so we can do
+// lvl 2 (datalink) stuff and so forth
+
+int tun_alloc(char* dev) {
+	struct ifreq ifr;
+	int fd, err;
+	return 0;
 }
+
 
